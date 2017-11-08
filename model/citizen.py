08b7321932136cab1cff12de47ba86ef5786e3be
August Kørvell <august@aksoftware.dk>
2017-11-08 14:43:53 +0100

    Added IDs to all users and startet put alarm

--- model/citizen.py
+++ model/citizen.py
@@ -4,8 +4,8 @@ from model.user import *
 class Citizen(User):
     'A specialized user, representing a contact'
 
-    def __init__(self, name, email, username, contacts, devices, settings):
-        super().__init__(name, email, username)
+    def __init__(self, id, name, email, username, contacts, devices, settings):
+        super().__init__(id, name, email, username)
         self.contacts = contacts
         self.devices = devices
         self.settings = settings
--- model/user_admin.py
+++ model/user_admin.py
@@ -4,5 +4,5 @@ from model.user import *
 class UserAdmin(User):
     'A specialized user, representing a contact'
 
-    def __init__(self, name, email, username):
-        super().__init__(name, email, username)
+    def __init__(self, id, name, email, username):
+        super().__init__(id, name, email, username)
