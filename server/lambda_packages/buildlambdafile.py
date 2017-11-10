23ac15b5db4c46bfb112f09d5edf35c11ff1e8e8
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2017-11-10 12:26:53 +0100

    improved buildlambdafile.py and added comments

--- server/lambda_packages/buildlambdafile.py
+++ server/lambda_packages/buildlambdafile.py
@@ -1,23 +1,44 @@
-# Arguments: foldername filename
-import os
-import sys
-import zipfile
+from os import path, makedirs
 from shutil import copyfile, copytree, make_archive, rmtree
 
+# When more lambda functions are made, append them below.
+# We do it this way just to make everything easier.
+# When requiring more than one file not part of the standard packed library, include them in the list.
+# The name of the output directory and zip file should be the last element in the list.
+# The first file in each list must be the main file
+file_folder_lists = [["activate_alarm.py", "activate_alarm"], ["put_alarm.py", "update_alarm"]]
 
-foldername = input("foldername")
-filename = input("filename")
+for file_folder_list in file_folder_lists:
+    # We create a folder, in which we put everything to upload with the lambda
+    # Remember that the folder name is the last name in the list
+    folder_name = file_folder_list[-1]
 
-if os.path.exists(foldername):
-    rmtree(foldername)
+    # If folder already exist, remove them
+    if path.exists(folder_name):
+        rmtree(folder_name)
 
-os.makedirs(foldername)
+    # Recreate folder
+    makedirs(folder_name)
+
+    # copy the main file over, and rename it
+    copyfile("../" + file_folder_list[0], folder_name + "/" + "lambda_function.py")
+
+    # Copy other specified files to the output directory
+    for file_name in file_folder_list[:-1]:
+        copyfile("../" + file_name, folder_name + "/" + file_name)
+
+    # connect_str is a private file containing connectivity info for the database
+    # This file should only be present locally on the computer packing the files
+    copyfile("lambda_package_resources/connect_str.py", folder_name + "/" + "connect_str.py")
+
+    # Copy the respond method. This is used to properly construct the response messages
+    copyfile("lambda_package_resources/respond.py", folder_name + "/" + "respond.py")
+
+    # Since AWS doesn't provide a library for interacting with PostgreSQL, we have to provide it ourselves
+    copytree("lambda_package_resources/psycopg2", folder_name + "/" + "psycopg2")
+
+    # Finally zip the output folder. The zip file can then be uploaded to the lambda function
+    make_archive(folder_name, "zip", folder_name)
 
 
-copyfile("../" + filename, foldername + "/" + "lambda_function.py")
 
-copyfile("lambda_package_resources/connect_str.py", foldername + "/" + "connect_str.py")
-copyfile("lambda_package_resources/respond.py", foldername + "/" + "respond.py")
-copytree("lambda_package_resources/psycopg2", foldername + "/" + "psycopg2")
-
-make_archive(foldername, "zip", foldername)
