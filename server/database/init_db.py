43fc331ad2d81a4916e5cd9cc7f134e6258a33fd
August Kørvell <august@aksoftware.dk>
2017-11-22 11:28:03 +0100

    Changed Alarm to have user as primary key

--- server/database/init_db.py
+++ server/database/init_db.py
@@ -5,25 +5,25 @@ try:
     conn = psycopg2.connect(connect_str)
     cursor = conn.cursor()
 
-    cursor.execute("CREATE TYPE userrole AS ENUM ('contact', 'citizenAdmin', 'citizen')");
-    cursor.execute("CREATE TYPE devicetype AS ENUM ('app', 'smartAssistance')");
+    cursor.execute("CREATE TYPE userrole AS ENUM ('contact', 'citizenAdmin', 'citizen')")
+    cursor.execute("CREATE TYPE devicetype AS ENUM ('app', 'smartAssistance')")
 
     # Database setup
     cursor.execute(
         "CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(50), password varchar(200), salt varchar(50), name varchar(255), email varchar(255), role userrole);")
 
-    cursor.execute("CREATE TABLE contact (userID int REFERENCES users(id) PRIMARY KEY, phone varchar(55));");
+    cursor.execute("CREATE TABLE contact (userID int REFERENCES users(id) PRIMARY KEY, phone varchar(55));")
     cursor.execute(
-        "CREATE TABLE citizen (userID int REFERENCES users(id) PRIMARY KEY, address varchar(255), city varchar(255), postnr varchar(55), managedBy int REFERENCES users(id));");
+        "CREATE TABLE citizen (userID int REFERENCES users(id) PRIMARY KEY, address varchar(255), city varchar(255), postnr varchar(55), managedBy int REFERENCES users(id));")
 
-    cursor.execute("CREATE TABLE device (id SERIAL PRIMARY KEY, type devicetype);");
+    cursor.execute("CREATE TABLE device (id SERIAL PRIMARY KEY, type devicetype);")
     cursor.execute(
-        "CREATE TABLE hasa (userID int REFERENCES users(id), deviceID int REFERENCES device(id), PRIMARY KEY(userID, deviceID));");
+        "CREATE TABLE hasa (userID int REFERENCES users(id), deviceID int REFERENCES device(id), PRIMARY KEY(userID, deviceID));")
     cursor.execute(
-        "CREATE TABLE associateswith (citizenID int REFERENCES citizen(userID), contactID int REFERENCES contact(userID), PRIMARY KEY(citizenID, contactID));");
+        "CREATE TABLE associateswith (citizenID int REFERENCES citizen(userID), contactID int REFERENCES contact(userID), PRIMARY KEY(citizenID, contactID));")
 
     cursor.execute(
-        "CREATE TABLE alarm (id SERIAL PRIMARY KEY, status int, activatedBy int REFERENCES citizen(userID))");
+        "CREATE TABLE alarm (status int, activatedBy int PRIMARY KEY REFERENCES citizen(userID))")
 
     conn.commit()
     cursor.close()
