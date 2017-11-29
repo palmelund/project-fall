34b63265692014910bc08ca93fdc192ec3fa25e4
August Kørvell <august@aksoftware.dk>
2017-11-29 15:06:27 +0100

    IFTTT and add device

--- model/device.py
+++ model/device.py
@@ -5,18 +5,22 @@ from database import database_manager
 class DeviceType(Enum):
     App = "App"
     PersonalAssistance = "PersonalAssistance"
+    IFTTT = "IFTTT"
 
 
 class Device:
 
     def __init__(self, id, type, content):
-        self.type = type
+        self.type = DeviceType(type)
         self.id = id
         self.content = content
 
     @staticmethod
     def get(deviceID):
         return database_manager.get_device(deviceID)
+
+    def put(self, user):
+        database_manager.set_device(self, user)
 
 
 def deserialize(mapping):
