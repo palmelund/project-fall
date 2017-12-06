77c5814e9a168a735d3697431ae9c93960c6e7fe
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2017-12-06 13:17:22 +0100

    Fixed scheme?

--- model/schemas.py
+++ model/schemas.py
@@ -2,6 +2,7 @@ from marshmallow import Schema, fields, post_load
 from model import user
 from model import device
 from model import alarm
+from pprint import pprint
 
 
 class DeviceSchema(Schema):
@@ -12,7 +13,7 @@ class DeviceSchema(Schema):
 
     @post_load
     def make_device(self, data):
-        return device.Device(**data)
+        return device.Device(data["id"], data["devicetype"], data["messagetype"], data["content"])
 
 
 class UserSchema(Schema):
@@ -23,18 +24,27 @@ class UserSchema(Schema):
 
     @post_load
     def make_user(self, data):
-        return user.User(**data)
+        pprint(data)
+        return user.User(data["id"], data["name"], data["email"], data["role"])
 
 
-class ContactSchema(UserSchema):
+class ContactSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    email = fields.Str()
+    role = fields.Str()
     devices = fields.Nested(DeviceSchema, many=True, missing=[])
 
     @post_load
     def make_contact(self, data):
-        return user.Contact(**data)
+        return user.Contact(data["id"], data["name"], data["email"], data["devices"])
 
 
-class CitizenSchema(UserSchema):
+class CitizenSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    email = fields.Str()
+    role = fields.Str()
     contacts = fields.Nested(ContactSchema, many=True, missing=[])
     devices = fields.Nested(DeviceSchema, many=True, missing=[])
     address = fields.Str()
@@ -43,21 +53,30 @@ class CitizenSchema(UserSchema):
 
     @post_load
     def make_citizen(self, data):
-        return user.Citizen(**data)
+        return user.Citizen(data["id"], data["name"], data["email"], data["contacts"], data["devices"], data["address"], data["city"], data["postnr"])
 
 
-class CitizenAdminSchema(UserSchema):
+class CitizenAdminSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    email = fields.Str()
+    role = fields.Str()
     citizens = fields.Nested(CitizenSchema, many=True, missing=[])
 
     @post_load
     def make_citizen_admin(self, data):
-        return user.CitizenAdmin(**data)
+        return user.CitizenAdmin(data["id"], data["name"], data["email"], data["citizens"])
 
 
-class UserAdminSchema(UserSchema):
+class UserAdminSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    email = fields.Str()
+    role = fields.Str()
+
     @post_load
     def make_user_admin(self, data):
-        return user.UserAdmin(**data)
+        return user.UserAdmin(data["id"], data["name"], data["email"])
 
 
 class AlarmSchema(Schema):
@@ -67,4 +86,4 @@ class AlarmSchema(Schema):
 
     @post_load
     def make_alarm(self, data):
-        return alarm.Alarm(**data)
+        return alarm.Alarm(data["status"], data["activatedby"], data["responder"])
