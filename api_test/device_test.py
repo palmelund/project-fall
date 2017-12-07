b7209084d3e7b968eefd9dbaa8ca89ebf6d611c4
August Kørvell <august@aksoftware.dk>
2017-12-07 13:38:32 +0100

    asdfghjkl

--- api_test/device_test.py
+++ api_test/device_test.py
@@ -42,6 +42,19 @@ class DeviceTestCase(unittest.TestCase):
 
             self.assertDictEqual(json.loads(_device.serialize().replace("'", "\"")), json.loads(_citizen_response.devices[0].serialize().replace("'", "\"")))
 
+        def test_device_delete(self):
+            _device = Device(-1, device.alexa, "input", {"userid": "abc"})
+            device_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/citizen/" + str(self._citizen.id) + "/device"
+            device_header = {"device": _device}
+            _device = device.deserialize(get_response(requests.post(device_uri, headers=device_header)))
+
+            user_get_header = {"email": "testington@tester.dk", "password": "1234"}
+            user_get_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/citizen/" + str(self._citizen.id)
+
+            _citizen_response = user.deserialize(get_response(requests.get(user_get_uri, headers=user_get_header)))
+
+            self.assertIsNone(_citizen_response.devices[0])
+            
 
 if __name__ == '__main__':
     unittest.main()
