131f4ea4f9391441546320537f7a7d12dee72fe7
August Kørvell <august@aksoftware.dk>
2017-12-11 17:16:25 +0100

    Added get_all_contacts

--- api_test/device_test.py
+++ api_test/device_test.py
@@ -25,23 +25,26 @@ class DeviceTestCase(unittest.TestCase):
             citizen_header = {"user": self._citizen.serialize().replace("\"", "\\\""), "password": "1234"}
 
             self._citizen = user.deserialize(get_response(requests.post("https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/user/", headers=citizen_header)))
+            print("Citizen id: " + str(self._citizen.id))
 
         def tearDown(self):
             user_delete_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/user/"
-            user_delete_header = {"user": self._citizen.serialize(), "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjM0IiwidXNlcl9yb2xlIjoidXNlckFkbWluIn0._zMvLSI7elh4ebv-1iaE8nXYdbzjyvpBNlDNhNSJMc0"}
-            # print("------------ delete user: " + str(self._citizen.serialize()))
-            # print("------------ delete response: " + str(requests.delete(user_delete_uri, headers=user_delete_header).text))
+            user_delete_header = {"user": self._citizen.serialize().replace("\"", "\\\""), "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjM0IiwidXNlcl9yb2xlIjoidXNlckFkbWluIn0._zMvLSI7elh4ebv-1iaE8nXYdbzjyvpBNlDNhNSJMc0"}
+            print("------------ delete user: " + str(self._citizen.serialize()))
+            print("------------ delete response: " + str(requests.delete(user_delete_uri, headers=user_delete_header).text))
 
         def test_device_post(self):
             _device = device.AlexaDevice(-1, "temp_token1234")
             device_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/citizen/" + str(self._citizen.id) + "/device"
             # print(_device.serialize())
             device_header = {"device": _device.serialize(), "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjM0IiwidXNlcl9yb2xlIjoidXNlckFkbWluIn0._zMvLSI7elh4ebv-1iaE8nXYdbzjyvpBNlDNhNSJMc0"}
+            print("Device: " + _device.serialize())
             _device = device.deserialize(get_response(requests.post(device_uri, headers=device_header)))
 
             user_get_header = {"email": "testington@tester.dk", "password": "1234"}
             user_get_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/user/"
 
+            print("user get: ")
             _citizen_response = user.deserialize(get_response(requests.get(user_get_uri, headers=user_get_header)))
 
             self.assertDictEqual(json.loads(_device.serialize().replace("'", "\"")), json.loads(_citizen_response.devices[0].serialize().replace("'", "\"")))
@@ -50,10 +53,10 @@ class DeviceTestCase(unittest.TestCase):
             _device = device.AlexaDevice(-1, "temp_token1234")
             device_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/citizen/" + str(self._citizen.id) + "/device"
             device_header = {"device": _device.serialize(), "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjM0IiwidXNlcl9yb2xlIjoidXNlckFkbWluIn0._zMvLSI7elh4ebv-1iaE8nXYdbzjyvpBNlDNhNSJMc0"}
-            _device = device.deserialize(get_response(requests.delete(device_uri, headers=device_header)))
+            print("Delete" + str(device.deserialize(get_response(requests.delete(device_uri, headers=device_header))).__dict__))
 
             user_get_header = {"email": "testington@tester.dk", "password": "1234"}
-            user_get_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/citizen/" + str(self._citizen.id)
+            user_get_uri = "https://prbw36cvje.execute-api.us-east-1.amazonaws.com/dev/user/"
 
             _citizen_response = user.deserialize(get_response(requests.get(user_get_uri, headers=user_get_header)))
 
--- server/contact_get.py
+++ server/contact_get.py
@@ -6,14 +6,14 @@ def lambda_handler(event, context):
     try:
         id = event["id"]
     except:
-        return respond("400", user.User(-1, "", "", "").serialize())
+        return respond("400", user.User(-1, "", "", "userAdmin").serialize())
 
     if not id:
-        return respond("400", user.User(-1, "", "", "").serialize())
+        return respond("400", user.User(-1, "", "", "userAdmin").serialize())
 
     usr = user.Contact.get(id)
 
     if not usr or usr.id == -1:
-        return respond("400", user.User(-1, "", "", "").serialize())
+        return respond("400", user.User(-1, "", "", "userAdmin").serialize())
     else:
         return respond("200", usr.serialize())
