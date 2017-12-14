fad9ef2a483f8538bca941bae4c1c523ace18901
r5hej <charliebyrdam@gmail.com>
2017-12-14 15:50:50 +0100

    Control panel, device and update user information.
    Finished functionality to add and remove devices to the currently
    viewing citizen.
    Still having problems with sending the updates users information to the
    server.

--- control_panel/js/main.js
+++ control_panel/js/main.js
@@ -2,7 +2,11 @@
 let baseURL = 'https://prbw36cvje.execute-api.us-east-1.amazonaws.com';
 let user = new Object();
 let container = document.getElementById('container');
-let contactlst = [];
+let contactLst = [];
+let citizenLst = [];
+let currentCitizen;
+let oldSearchQuery = "";
+let deviceLst = [];
 
 toastr.options = {
     "positionClass" : "toast-bottom-right",
@@ -11,42 +15,53 @@ toastr.options = {
 
 window.onload = RenderLoginView();
 
+
 function GetContactList() {
     $.ajax({
         url: baseURL + '/dev/contact',
         type: 'GET',
         success: function(response) {
             if (response.statusCode !== "200") {
+                console.log("Couldn't fetch contact list");
                 toastr.error("Noget gik galt. Prøv at logge ind igen");
                 return;
             }
             let json = JSON.parse(response.body);
 
             for (let i = 0; i < json.length; i++) {
-                contactlst[i] = json[i];
+                contactLst[i] = json[i];
             }
         },
         error: function(response) {
+            console.log("Couldn't fetch contact list");
             console.log("Couldn't fetch contactlist");
         }
     });
 }
 
-function AddNewCitizen(){
-    let form = $("#citizen-form");
-
+function GetCitizenList() {
     $.ajax({
-        url: '/fisk/fisk',
-        type: 'POST',
-        data: form.serialize(),
-        success: function(respose) {
-            console.log("success");
+        url: baseURL + '/dev/citizen',
+        type: 'GET',
+        success: function(response) {
+            if (response.statusCode !== "200") {
+                console.log("Couldn't fetch citizen list");
+                toastr.error("Noget gik galt. Prøv at logge ind igen");
+                return;
+            }
+
+            let json = JSON.parse(response.body);
+            for (let i = 0; i < json.length; i++) {
+                citizenLst[i] = json[i];
+            }
+
+            RenderCitizenList();
         },
         error: function(response) {
-            console.log("error");
+            console.log("Couldn't fetch citizen list");
+            toastr.error("Noget gik galt. Prøv at logge ind igen");
         }
     });
-    form[0].reset();
 }
 
 function AddNewContact (id, name) {
@@ -58,11 +73,13 @@ function AddNewContact (id, name) {
         id: id
     });
 
-    contactlst.find(function (contact) {
+    contactLst.find(function (contact) {
         if (contact.id == id){
-            user.body.contacts[user.body.contacts.length] = contact;
+            currentCitizen.contacts[currentCitizen.contacts.length] = contact;
         }
     });
+
+    RenderSearchContact();
 }
 
 function RenderContactList () {
@@ -70,36 +87,50 @@ function RenderContactList () {
     let template = JsT.loadById('template-contact-row');
 
     contactList.innerHTML = "";
-    for (i = 0; i < user.body.contacts.length; i++){
+    for (i = 0; i < currentCitizen.contacts.length; i++){
         contactList.innerHTML += template.render({
-            name: user.body.contacts[i].name,
-            id: user.body[i].id
+            name: currentCitizen.contacts[i].name,
+            id: currentCitizen.contacts[i].id
         });
     }
+    console.log(contactLst);
 }
 
-function RemoveContact (phone) {
+function RemoveContact (id) {
+    let index;
+    for (let i = 0; i < currentCitizen.contacts.length; i++) {
+        if (currentCitizen.contacts[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+
+    currentCitizen.contacts.splice(index, 1);
+    RenderContactList();
+    RenderSearchContact();
 }
 
-function SearchForContact(event) {
-    if (event.key !== 'Enter') { return; }
-
-    let query = document.getElementById('search-contact-box').value;
+function RenderSearchContact(oldQuery = null) {
     let tableOut = document.getElementById('search-result');
     let template = JsT.loadById('template-search-result-row');
     let result = [];
 
-    for (let i = 0, count = 0; i < contactlst.length; i++) {
-        let exists = false;
-        match = (contactlst[i].email).includes(query);
+    let query = oldQuery == null ?
+        document.getElementById('search-contact-box').value :
+        oldQuery;
 
-        user.body.contacts.find(function (contacts) {
-            if (contacts.id == contactlst[i].id){
+    for (let i = 0, count = 0; i < contactLst.length; i++) {
+        let exists = false;
+        match = (contactLst[i].email).includes(query);
+
+        currentCitizen.contacts.find(function (contacts) {
+            if (contacts.id == contactLst[i].id) {
                 exists = true;
             }
         });
-        if (match && !exists){
-            result[count] = contactlst[i];
+
+        if (match && !exists) {
+            result[count] = contactLst[i];
             count++;
         }
     }
@@ -113,45 +144,145 @@ function SearchForContact(event) {
     }
 }
 
+function SearchForContact(event) {
+    if (event.key !== 'Enter') { return; }
 
-function RenderCitizenInfo (phone) {
-    // let infoBox = document.getElementById('citizen-info');
-    // let template = JsT.loadById('template-citizen-info');
+    oldSearchQuery = null;
+    RenderSearchContact();
+}
 
-    $.ajax({
-        url: 'get/url',
-        type: 'GET',
-        data: {data: phone},
-        success: function (response){
-            let infoBox = document.getElementById('citizen-info');
-            let template = JsT.loadById('template-citizen-info');
-            infoBox.className += " box";
+function RenderCitizenList() {
+    lst = document.getElementById('citizen-list');
+    template = JsT.loadById('template-citizen-item');
 
-            infoBox.innerHTML = template.render({
-                citizenName: user.body.name,
-                email: user.body.email,
-                address: user.body.address,
-                city: user.body.city,
-                postalCode: user.body.postnr
-            });
-        },
-        error: function (response){
-            console.log('no citizen found');
+    for (let i = 0; i < citizenLst.length; i++) {
+        lst.innerHTML += template.render({
+            name: citizenLst[i].name,
+            id: citizenLst[i].id
+        });
+    }
+}
 
-            let infoBox = document.getElementById('citizen-info');
-            let template = JsT.loadById('template-citizen-info');
-            infoBox.className += " box";
+function RenderCitizenInfo (id) {
+    let infoBox = document.getElementById('citizen-info');
+    let template = JsT.loadById('template-citizen-info');
+    infoBox.className += " box";
 
-            infoBox.innerHTML = template.render({
-                citizenName: user.body.name,
-                email: user.body.email,
-                address: user.body.address,
-                city: user.body.city,
-                postalCode: user.body.postnr
-            });
+    let citizen = citizenLst.find(function (citizen) {
+        if (citizen.id == id) {
+            return citizen;
         }
     });
+    currentCitizen = citizen;
+    deviceLst = currentCitizen.devices;
+
+    infoBox.innerHTML = template.render({
+        citizenName: citizen.name,
+        email: citizen.email,
+        address: citizen.address,
+        city: citizen.city,
+        postalCode: citizen.postnr
+    });
     RenderContactList();
+}
+
+function UpdateCitizenToServer() {
+    // console.log(currentCitizen);
+    let id = currentCitizen.id;
+    let name = currentCitizen.name;
+    let email = currentCitizen.email;
+    let add = currentCitizen.address;
+    let city = currentCitizen.city;
+    let zip = currentCitizen.postnr;
+    let token = currentCitizen.token;
+    let devices = JSON.stringify(deviceLst).replace(/\"/g, '\'');
+    let contacts = JSON.stringify(currentCitizen.contacts).replace(/\"/g, '\'');
+
+
+    userData = "{'id': '" + id + "', " + "'name': '" + name + "', 'email': '" + email +
+        "', 'role': 'citizen', 'address': '" + add + "', 'city': '" + city +
+        "', 'postnr': '"+ zip + "', 'devices': '" + devices + "', 'contacts': '" +
+        contacts + "', 'token': '" + token + "}";
+
+
+    // console.log(userData);
+
+    $.ajax({
+        url: baseURL + '/dev/user',
+        contentType: 'application/json',
+        headers: {'user': userData},
+        type: 'PUT',
+        success: function(response) {
+            console.log("sucess");
+            if (response.statusCode !== "200") {
+                toastr.error(currentCitizen.name + " blev ikke gemt");
+                return;
+            }
+
+            console.log(response);
+
+            toastr.success(currentCitizen.name + "blev gemt");
+        },
+        error: function(response) {
+            console.log(response);
+            console.log("error");
+            toastr.error(currentCitizen.name + " blev ikke gemt");
+        }
+    });
+}
+
+function RadioButton(device) {
+    let alexa = document.getElementById('radio-alexa');
+    let ifttt = document.getElementById('radio-ifttt');
+
+
+    if (device === "alexa") {
+        ifttt.removeAttribute('checked');
+        alexa.setAttribute('checked', 'checked');
+    }
+    else {
+        alexa.removeAttribute('checked');
+        ifttt.setAttribute('checked', 'checked');
+    }
+}
+
+function AddDevice() {
+    let device = $('#add-device').find('input[name=device]:checked').val();
+    let token = $('#add-device').find('input[name=token]').val();
+    let obj = new Object();
+
+    obj.devicetype = device;
+    obj.token = token;
+    obj.id = deviceLst.length;
+    deviceLst[obj.id] = obj;
+
+    RenderDeviceList();
+}
+
+function RemoveDevice(id) {
+    let index;
+    for (let i = 0; i < deviceLst.length; i++) {
+        if (deviceLst[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+
+    deviceLst.splice(index, 1);
+    RenderDeviceList();
+}
+
+function RenderDeviceList() {
+    let table = document.getElementById('device-table');
+    let template = JsT.loadById('template-device-row');
+
+    table.innerHTML = "";
+    for (let i = 0; i < deviceLst.length; i++) {
+        table.innerHTML += template.render({
+            devicetype: deviceLst[i].devicetype,
+            id: deviceLst[i].id
+        });
+    }
 }
 
 // login
@@ -180,7 +311,7 @@ function RenderRegisterContactView() {
     view.innerHTML = template.render();
 }
 
-function Login () {
+function Login() {
     let form = $('form').serialize();
 
     $.ajax({
@@ -198,7 +329,11 @@ function Login () {
                 container.classList.add('wrapper');
                 container.classList.remove('container');
                 container.innerHTML = template.render();
+
+                console.log(user);
+
                 GetContactList();
+                GetCitizenList();
             }
             else if (user.statusCode == 400 && user.body.role == 'citizen') {
                 toastr.error("Email eller kode passer ikke");
@@ -215,6 +350,7 @@ function Login () {
         }
     });
 }
+
 
 function RegisterNewUser(role) {
     let pass1 = $('#add-new-user-form').find('input[name=password]').val();
